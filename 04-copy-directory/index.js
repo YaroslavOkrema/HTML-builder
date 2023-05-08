093f3a5bb1f431327aa670c913d57db603b5e851
YaroslavOkrema <okrema.y22@fpm.dnu.edu.ua>
2023-05-08 19:19:54 +0300

    feat: add solution 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,33 @@
+const fs = require('fs');
+const path = require('path');
 
+const dirPath = path.join(__dirname, 'files');
+
+fs.readdir(dirPath, { withFileTypes: true }, (err, files) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  const targetDirPath = path.join(__dirname, 'files2');
+  fs.mkdir(targetDirPath, { recursive: true }, err => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    files.forEach(file => {
+      if (file.isFile()) {
+        const sourceFilePath = path.join(dirPath, file.name);
+        const targetFilePath = path.join(targetDirPath, file.name);
+        fs.copyFile(sourceFilePath, targetFilePath, err => {
+          if (err) {
+            console.error(`Ошибка копирования ${file.name}:`, err);
+          } else {
+            console.log(`File ${file.name} скопирован успешно`);
+          }
+        });
+      }
+    });
+  }); 
+});
