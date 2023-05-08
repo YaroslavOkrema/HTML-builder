212cb77e0db157f86c30fc9cc1c283f7d12c9a64
YaroslavOkrema <okrema.y22@fpm.dnu.edu.ua>
2023-05-08 19:17:47 +0300

    feat: add solution 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'text.txt');
+
+const stream = fs.createReadStream(filePath, 'utf8');
+stream.on('data', (dataChunk) => {
+    console.log(dataChunk);
+})
